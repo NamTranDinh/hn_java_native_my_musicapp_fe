1faedb13d146114d8401915d6477c6b3478ab078
SonNguyen <son15052002@gmail.com>
2023-04-02 04:22:45 +0700

    - update ServerConfig

--- app/src/main/java/com/aptech/mymusic/config/ServerConfig.java
+++ app/src/main/java/com/aptech/mymusic/config/ServerConfig.java
@@ -2,6 +2,7 @@ package com.aptech.mymusic.config;
 
 public class ServerConfig {
 
-    public static final String BASE_API = "http://192.168.1.10:8080/api/";
+    // public static final String BASE_API = "http://192.168.1.10:8080/api/";
+    public static final String BASE_API = "https://mymusicserver-aws-production.up.railway.app/api/";
 
 }
