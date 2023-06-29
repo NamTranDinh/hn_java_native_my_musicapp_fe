84ca35538cb884a8d52f38fe63cc5657446dc689
Trần Đình Nam(ASIA) <namtd@aht-asia.com>
2023-06-29 21:49:37 +0700

    init

--- app/src/main/java/com/aptech/mymusic/config/ServerConfig.java
+++ app/src/main/java/com/aptech/mymusic/config/ServerConfig.java
@@ -5,4 +5,5 @@ public class ServerConfig {
     // public static final String BASE_API = "http://192.168.1.10:8080/api/";
     public static final String BASE_API = "https://mymusicserver-aws-production.up.railway.app/api/";
 
+//    public static final String BASE_API = "http://192.168.22.2:8080/api/";
 }
